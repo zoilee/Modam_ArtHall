c196aac28a986d81dfd3c2bf63b237613171d6c1
zoilee <ohsj2337@gmail.com>
2024-12-04 17:41:05 +0900

    log적립 db 테이블 추가됬어요

--- devlop/Modam_ArtHall/src/main/java/com/arthall/modam/service/PerformanceService.java
+++ devlop/Modam_ArtHall/src/main/java/com/arthall/modam/service/PerformanceService.java
@@ -90,10 +90,10 @@ public class PerformanceService {
     public Page<PerformancesEntity> getPastPerformances(Pageable pageable) {
         // Repository에서 데이터 가져오기 (이미 최신순 정렬 적용됨)
         Page<PerformancesEntity> pastPerformances = performancesRepository.findPastPerformances(pageable);
-    
+
         // 디버깅 로그
         System.out.println("Fetched Performances: " + pastPerformances.getContent().size());
-    
+
         // 날짜 포맷팅
         pastPerformances.getContent().forEach(performance -> {
             if (performance.getStartDate() != null) {
@@ -105,7 +105,7 @@ public class PerformanceService {
                         dateFormatter.format(performance.getEndDate().toLocalDate()));
             }
         });
-    
+
         return pastPerformances;
     }
 
@@ -165,8 +165,6 @@ public class PerformanceService {
     public List<PerformancesEntity> getPerformancesByDate(Date startDate, Date endDate) {
         return performancesRepository.findByStartDateBeforeAndEndDateAfter(startDate, endDate);
     }
-    
-
 
     // 모든 공연의 예약 현황 정보 가져오기 (내림차순 정렬)
     public List<PerformancesDto> getPerformancesWithReservationRate() {
@@ -218,4 +216,10 @@ public class PerformanceService {
         }
     }
 
+    public String findTitleById(int id) {
+        PerformancesEntity performance = performancesRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("공연을 찾을 수 없습니당"));
+        String title = performance.getTitle();
+        return title;
+    }
 }
