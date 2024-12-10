631ad0aa95e67f0bd9561b5daa74a21fca9a7f6f
OurOh <ahn980321@gmail.com>
2024-12-10 13:11:39 +0900

    어플리케이션
    어플리케이션

--- devlop/Modam_ArtHall/src/main/java/com/arthall/modam/ModamApplication.java
+++ devlop/Modam_ArtHall/src/main/java/com/arthall/modam/ModamApplication.java
@@ -10,14 +10,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 public class ModamApplication {
 
     public static void main(String[] args) {
-        // 명시적으로 .env 파일 로드
-        Dotenv dotenv = Dotenv.configure()
-                .directory("C:/springboot/Modam_ArtHall") // .env 파일 경로 설정
-                .load();
-
-        // 환경 변수 등록
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
+       
         SpringApplication.run(ModamApplication.class, args);
     }
 }
