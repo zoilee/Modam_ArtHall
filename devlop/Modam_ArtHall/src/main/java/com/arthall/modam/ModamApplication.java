80e33d714d341b208df943de210674899da9cc66
OurOh <ahn980321@gmail.com>
2024-12-10 11:28:44 +0900

    1210 fileService
    1210 fileService

--- devlop/Modam_ArtHall/src/main/java/com/arthall/modam/ModamApplication.java
+++ devlop/Modam_ArtHall/src/main/java/com/arthall/modam/ModamApplication.java
@@ -1,5 +1,6 @@
 package com.arthall.modam;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,8 +9,15 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @EntityScan(basePackages = {"com.arthall.modam.entity"})
 public class ModamApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ModamApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // 명시적으로 .env 파일 로드
+        Dotenv dotenv = Dotenv.configure()
+                .directory("C:/springboot/Modam_ArtHall") // .env 파일 경로 설정
+                .load();
 
+        // 환경 변수 등록
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(ModamApplication.class, args);
+    }
 }
