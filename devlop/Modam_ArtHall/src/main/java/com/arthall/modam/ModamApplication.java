af2e2baa0568bce5f8e064281482cfe3a2bc9508
zoilee <ohsj2337@gmail.com>
2024-12-10 13:11:15 +0900

    reservConfirm 스타일 변경

--- devlop/Modam_ArtHall/src/main/java/com/arthall/modam/ModamApplication.java
+++ devlop/Modam_ArtHall/src/main/java/com/arthall/modam/ModamApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.arthall.modam.entity"})
+@EntityScan(basePackages = { "com.arthall.modam.entity" })
 public class ModamApplication {
-
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         
+=======
+>>>>>>> Stashed changes
         SpringApplication.run(ModamApplication.class, args);
     }
 }
