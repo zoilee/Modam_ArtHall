0e4df117b39f45b2903aa2346d19a5e1e4cce3ec
parkminjung <syapmj1218@gmail.com>
2024-12-11 09:54:05 +0900

    서버 실행 문제 수정
    서버 실행 문제 해결

--- devlop/Modam_ArtHall/src/main/java/com/arthall/modam/ModamApplication.java
+++ devlop/Modam_ArtHall/src/main/java/com/arthall/modam/ModamApplication.java
@@ -1,14 +1,13 @@
 package com.arthall.modam;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = { "com.arthall.modam.entity" })
+@EntityScan(basePackages = {"com.arthall.modam.entity"})
 public class ModamApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(ModamApplication.class, args);
     }
 }
