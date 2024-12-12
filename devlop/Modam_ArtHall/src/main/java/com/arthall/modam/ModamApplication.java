f51e625668c72d4b8e4cd2104996ed3a90c45f97
zoilee <ohsj2337@gmail.com>
2024-12-12 15:12:31 +0900

    Update ModamApplication.java

--- devlop/Modam_ArtHall/src/main/java/com/arthall/modam/ModamApplication.java
+++ devlop/Modam_ArtHall/src/main/java/com/arthall/modam/ModamApplication.java
@@ -4,18 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EntityScan(basePackages = { "com.arthall.modam.entity" })
 public class ModamApplication {
 
     public static void main(String[] args) {
-        // Dotenv dotenv = Dotenv.configure()
-        // .directory(".//")
-        // .load();
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .load();
 
-        // // 프로퍼티 설정 .
-        // dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(),
-        // entry.getValue()));
+        // 프로퍼티 설정 .
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(),
+                entry.getValue()));
 
         SpringApplication.run(ModamApplication.class, args);
     }
