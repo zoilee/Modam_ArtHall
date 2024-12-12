b99a8c996a6fac59b040bb48cb487615c8262ba8
zoilee <ohsj2337@gmail.com>
2024-12-12 13:54:54 +0900

    env 설정 test

--- devlop/Modam_ArtHall/src/main/java/com/arthall/modam/ModamApplication.java
+++ devlop/Modam_ArtHall/src/main/java/com/arthall/modam/ModamApplication.java
@@ -11,7 +11,7 @@ public class ModamApplication {
 
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure()
-                .directory("./")
+                .directory(".//")
                 .load();
 
         // 프로퍼티 설정
